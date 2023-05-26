5b11ecd0f8df337d5b6ff88c9f60a8c8eef7fb83
ShaiBM123 <shaibenmichael@gmail.com>
2023-05-26 17:41:08 +0300

    trial and error - stage 1

---
+++ src/test1.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { isIphoneX } from 'react-native-iphone-x-helper';
