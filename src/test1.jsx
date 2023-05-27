0aad3fced29b8d9cd4516ba2f22da8b7eb5f989e
ShaiBM123 <shaibenmichael@gmail.com>
2023-05-27 20:29:07 +0300

    storybook installation

--- src/test1.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import { isIphoneX } from 'react-native-iphone-x-helper';
